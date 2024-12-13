7b043a73d5b99c653b64f78363ccf6aba06785c9
Adil Aliyev <aliyev.adil.n@gmail.com>
2024-12-13 21:06:11 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    // grnrtics
+
+    //raw typr
+    // type safte
+    // java strongly
+
+    // name conversion
+    // e- element
+        // t - tipe
+        // k- key
+        // number
+
+
+
+    }
+}
